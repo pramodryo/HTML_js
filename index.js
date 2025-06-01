ab2d01efe605f6635e2f0f809448de2fc3fa4abf
pramod rathod <pramod.ryo@gmail.com>
2025-06-01 14:38:04 +0530

    added HTML and JS code

---
+++ index.js
@@ -0,0 +1,45 @@
+// 1. way to print in JS
+
+    // console.log("Hello World");
+    // alert("alert POP up");
+
+// 2. JavaScript variables
+// Js variable can be defined in four way
+// - automatically
+// - using var    var a=10
+// - using let    let a=10
+// - uisng const  const a=10
+
+
+// 3. Data Types in JavaScript
+
+// 1. String       var name= "pramod"
+// 2. Number       var x=12
+// 3. BigInt       
+// BigInt: in Js BigInt is new datatype that can be used to store interger value that are too big to be reprasented by 
+//normal JS Number
+
+// 4. Boolean      var x= true
+// 5. Undefined    var a;
+// 6. Null         var a=NULL;
+// 7. Symbol        
+// 8. Object
+
+// Object:  
+// const person={firstName:"pramod", LastName:"Rtahod"}
+
+// Array Object:
+// conat cars=["Volvo","BMW", "Saab"];
+
+// Date Object:
+//  const date: new date("2025-06-01");
+
+var result= 16 + "flower"
+console.log(result);   // NOTE: if number and string add that time JS will treat number as string
+
+// JS is dyanmic type: means same variable can hold different datatype
+ex: 
+var x=10;
+console.log(x);   // print: 10
+var x="string"
+console.log(x);   // print: string
